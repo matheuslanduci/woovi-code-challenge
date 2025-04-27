869e694228ad5e66bb6043ca3ca7439be0896ed4
matheuslanduci <matheuslanduci@gmail.com>
2025-04-27 20:41:04 -0300

    ci: fix redis

--- apps/server/src/broker.ts
+++ apps/server/src/broker.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis'
 
 export const broker = new Redis(
-  process.env.REDIS_HOST || 'redis://localhost:6379'
+  (process.env.REDIS_HOST || 'redis://localhost:6379') + '?family=0'
 )
